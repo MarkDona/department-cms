dcfdd40fb368a702a3a2d0fe73ec7a2fe877cb64
L0MAX <ankahdonatus@gmail.com>
2023-02-10 12:28:25 +0000

    updated Dues model, and Dues payment model

--- app/Models/Dues.php
+++ app/Models/Dues.php
@@ -4,8 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Dues extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $guarded = [];
 }
