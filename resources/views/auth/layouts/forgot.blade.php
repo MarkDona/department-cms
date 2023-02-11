24b354a025a9c7a87c28c133a7bb314074d6d51a
L0MAX <ankahdonatus@gmail.com>
2023-02-11 16:34:51 +0000

    updated forgot password, added footer links

--- resources/views/auth/layouts/forgot.blade.php
+++ resources/views/auth/layouts/forgot.blade.php
@@ -110,20 +110,20 @@
 
 
 <!-- BEGIN: Vendor JS-->
-<script src="../../../app-assets/vendors/js/vendors.min.js"></script>
+<script src="{{asset('donatus/app-assets/vendors/js/vendors.min.js')}}"></script>
 <!-- BEGIN Vendor JS-->
 
 <!-- BEGIN: Page Vendor JS-->
-<script src="../../../app-assets/vendors/js/forms/validation/jquery.validate.min.js"></script>
+<script src="{{asset('donatus/app-assets/vendors/js/forms/validation/jquery.validate.min.js')}}"></script>
 <!-- END: Page Vendor JS-->
 
 <!-- BEGIN: Theme JS-->
-<script src="../../../app-assets/js/core/app-menu.js"></script>
-<script src="../../../app-assets/js/core/app.js"></script>
+<script src="{{asset('donatus/app-assets/js/core/app-menu.js')}}"></script>
+<script src="{{asset('donatus/app-assets/js/core/app.js')}}"></script>
 <!-- END: Theme JS-->
 
 <!-- BEGIN: Page JS-->
-<script src="../../../app-assets/js/scripts/pages/auth-forgot-password.js"></script>
+<script src="{{asset('donatus/app-assets/js/scripts/pages/auth-forgot-password.js')}}"></script>
 <!-- END: Page JS-->
 
 <script>
