098f33fedb7ea09bd80fb1612ccec25bd7fb88bb
L0MAX <ankahdonatus@gmail.com>
2023-06-19 16:30:37 +0000

    corrected login error

--- routes/web.php
+++ routes/web.php
@@ -28,9 +28,10 @@ Route::get('/home', [HomeController::class, 'index'])->name('home');
 
 
 Route::get('/payments',[HomeController::class, 'get_duespayments'])->name('payments');
-Route::any('/verify-payment', [DuesPaymentController::class, 'verifyPayment'])->name('pay.verify');
+//Route::any('/verify-payment', [DuesPaymentController::class, 'verifyPayment'])->name('pay.verify');
 
 Route::get('/verify-payment/{reference}', [DuesPaymentController::class, 'verify'])->name('pay.verify');
+
 Route::get('/noticeboard',[HomeController::class, 'get_noticeboard'])->name('noticeboard');
 Route::get('/biodata',[HomeController::class, 'get_bio_data'])->name('biodata');
 Route::post('/update_profile',[HomeController::class, 'update_biodata'])->name('update_profile');
